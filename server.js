6550785779b4429d4ca0511f89e286986968b44b
Aryan107rn <aryannakade123@gmail.com>
2026-09-18 01:13:56 +0530

    successfully fetched driver from driver.js

--- server.js
+++ server.js
@@ -9,9 +9,22 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get("/drivers",(req,res)=>{
-res.json(drivers);
+    res.json(drivers);
+});
+
+app.get("/driver/:id",(req,res)=>{
+    const id=Number(req.params.id);
+    const driver = drivers.find((drivers)=>{
+        return driver.id === id ;
+    });
+    if(!driver){
+        return res.status(404).json({
+            message:"Driver not found";
+        })
+    }
+    res.json(driver);
 });
 
 app.listen(PORT,()=>{
-    console.log(`Server running on https://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
