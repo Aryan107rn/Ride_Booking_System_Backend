74a07a5628fdb182b9731aff00982644286f7847
Aryan107rn <aryannakade123@gmail.com>
2026-09-19 02:57:27 +0530

    update remaining

--- server.js
+++ server.js
@@ -4,55 +4,21 @@ import rides from "./data/rides.js";
 
 const app = express();
 
+// middleware
+
 const PORT = 3000;
 
-// Middleware
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.send("Welcome! Book your ride");
+// home route
+app.get("/",(req,res)=>{
+    res.send("This is ride booking site backend");
 });
 
-
-app.get("/driver", (req, res) => {
-    const { available } = req.query;
-
-    // If no query parameter is provided
-    // return all drivers
-    if (available === undefined) {
-        return res.json(drivers);
-    }
-
-    // Convert query string into boolean
-    const isAvailable = available === "true";
-
-    // Filter drivers
-    const filteredDrivers = drivers.filter((driver) => {
-        return driver.available === isAvailable;
-    });
-
-    res.json(filteredDrivers);
+app.get("/driver",(req,res)=>{
+    
 });
 
-
-
-app.get("/driver/:id", (req, res) => {
-    const id = Number(req.params.id);
-
-    const driver = drivers.find((driver) => {
-        return driver.id === id;
-    });
-
-    if (!driver) {
-        return res.status(404).json({
-            message: "Driver not found"
-        });
-    }
-
-    res.json(driver);
-});
-
-
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT,()=>{
+     console.log(`Server running on http://localhost:${PORT}`);
 });
