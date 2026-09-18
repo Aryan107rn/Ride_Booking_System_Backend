1c408bf8947a194df993ab7a70db3dc1b8ddcefe
Aryan107rn <aryannakade123@gmail.com>
2026-09-19 03:00:41 +0530

    completed simple route

--- server.js
+++ server.js
@@ -16,8 +16,35 @@ app.get("/",(req,res)=>{
 });
 
 app.get("/driver",(req,res)=>{
-    
+    const { available }=req.query;
+    if(available===undefined){
+        return res.json(drivers);
+    }
+    const isAvailable=available==="true";
+
+     const filteredDrivers = drivers.filter((driver) => {
+        return driver.available === isAvailable;
+    });
+
+    res.json(filteredDrivers);
 });
+
+app.get("/driver/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    const driver = drivers.find((driver) => {
+        return driver.id === id;
+    });
+
+    if (!driver) {
+        return res.status(404).json({
+            message: "Driver not found"
+        });
+    }
+
+    res.json(driver);
+});
+
 
 app.listen(PORT,()=>{
      console.log(`Server running on http://localhost:${PORT}`);
